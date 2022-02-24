be8e3f63a6b31cd2250212d272fad5613b7c8909
xusky69 <david.vivas@spectrio.com>
2022-02-24 16:45:30 -0500

    sync

---
+++ pages/events/index.js
@@ -0,0 +1,29 @@
+import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
+
+export default function EventsPage({ events }) {
+  console.log(events)
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events:events, revalidate: 1},
+  }
+
+}
---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
+import Link from 'next/link'
+
+export default function HomePage({ events }) {
+  console.log(events)
+  return (
+    <Layout>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+    {events.length > 0 && (
+      <Link href='/events'>
+        <a className='btn-secondary'>View All Events</a>
+      </Link>
+    )}
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events: events.slice(0,3), revalidate: 1},
+  }
+
+}
